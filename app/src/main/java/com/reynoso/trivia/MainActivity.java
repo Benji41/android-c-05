99a9b57af9fcb79417d3f72fdfe73b5b71202593
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-09-25 23:54:37 -0700

    Commit 2 +Se almacenaron los objetos en un arraylist, pero estos no son transportados a la MainActivity.java por terminos de asegurar el contenido y porque la tarea de obtener los objetos es asincrona.

--- app/src/main/java/com/reynoso/trivia/MainActivity.java
+++ app/src/main/java/com/reynoso/trivia/MainActivity.java
@@ -14,40 +14,24 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 import com.reynoso.trivia.controller.AppController;
+import com.reynoso.trivia.data.Repository;
 import com.reynoso.trivia.databinding.ActivityMainBinding;
+import com.reynoso.trivia.model.Question;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding mainBinding;
-    private String TAG = "MainActivity";
-    private String URL = "https://opentdb.com/api.php?amount=10&type=boolean";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,URL, null, new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONArray jsonArray = response.getJSONArray("results");
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "onErrorResponse: ", error);
-            }
-        });
         Utils.toast("WASSSSUP");
         Utils.snackBar(mainBinding.getRoot(),"Noe");
-        AppController.getInstance().addToRequestQueue(jsonObjectRequest);
+        List<Question> questionList= Repository.getQuestions();
     }
 }
--- app/src/main/java/com/reynoso/trivia/data/Repository.java
+++ app/src/main/java/com/reynoso/trivia/data/Repository.java
@@ -1,7 +1,53 @@
 package com.reynoso.trivia.data;
 
+import android.app.Application;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.reynoso.trivia.MainActivity;
+import com.reynoso.trivia.controller.AppController;
+import com.reynoso.trivia.model.Question;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Noé Benjamín Reynoso Aguirre on 9/25/2022.
  */
-public class Repository {
+public class Repository{
+    private static ArrayList<Question> questionArrayList = new ArrayList<>();
+    private static String URL = "https://opentdb.com/api.php?amount=10&type=boolean";
+
+    public static List<Question> getQuestions(){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,URL, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("results");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        questionArrayList.add(new Question(
+                                jsonArray.getJSONObject(i).getString("question"),jsonArray.getJSONObject(i).getBoolean("correct_answer")
+                        ));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(Repository.class.getName(), "onErrorResponse: ", error);
+            }
+        });
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest);
+        return questionArrayList;
+    }
 }
